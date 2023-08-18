efc781182bcaffe44bac791d28ac4fdc51147e35
Emily Svetcos <emilysvetcos@gmail.com>
2023-08-18 14:58:25 -0500

    downloaded DB

--- select_exercises.sql
+++ select_exercises.sql
@@ -6,6 +6,9 @@ DESCRIBE albums;
 
 SELECT * FROM albums ;
 
-SELECT * FROM albums WHERE name = 'Sgt Peppers Lonely Hearts Club';
+# SELECT * FROM albums WHERE name = 'Sgt Peppers Lonely Hearts Club';
 
 SELECT * FROM albums WHERE name = 'Nevermind';
+
+
+SELECT * FROM albums WHERE release_date = 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999;
