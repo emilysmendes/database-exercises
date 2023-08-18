145f6b00f1daa31a687989463b9c25f5f7ced05a
Emily Svetcos <emilysvetcos@gmail.com>
2023-08-18 14:04:51 -0500

    working on exercises

---
+++ select_exercises.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+SHOW tables;
+
+DESCRIBE albums;
+
+SELECT * FROM albums ;
+
+SELECT * FROM albums WHERE name = 'Sgt Peppers Lonely Hearts Club';
+
+SELECT * FROM albums WHERE name = 'Nevermind';
