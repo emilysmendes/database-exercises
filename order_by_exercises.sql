4e4ebc6c9200b22ff28f5f2dd494878b94e744be
Emily Svetcos <emilysvetcos@gmail.com>
2023-08-21 10:02:31 -0500

    first excercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -9,7 +9,8 @@ USE employees;
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
 
 
 
