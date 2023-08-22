847cff8293cb77744be044688dcad76290924bd4
Emily Svetcos <emilysvetcos@gmail.com>
2023-08-22 09:12:34 -0500

    finishing up lesson

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,8 +10,8 @@ Technique Leader
 Manager
 */
 
-# SELECT DISTINCT title
-# FROM titles;
+SELECT DISTINCT title
+FROM titles;
 
 
 /* TODO: Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
@@ -22,9 +22,34 @@ Erie
 Etalle
 */
 
-SELECT emp_no, first_name, last_name
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR '%E';
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR '%E'
+GROUP BY last_name;
+
+
+/*TODO: Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.*/
+
+SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' OR '%E';
 
 
+/*TODO: Find the unique last names with a 'q' but not 'qu'. Your results should be:
+Chleq
+Lindqvist
+Qiwen
+*/
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
+
+
+/*TODO: Add a COUNT() to your results and use GROUP BY to make it easier to find employees whose unusual name is shared with others.*/
 
